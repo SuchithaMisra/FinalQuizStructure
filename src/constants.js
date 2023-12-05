4dca2bb1b26f5b6c2fb21974270d517f455847f0
SuchithaMisra <suchitha.misra@uconn.edu>
2023-12-05 14:17:48 -0500

    https://github.com/SuchithaMisra/FinalQuizStructure.git

---
+++ src/constants.js
@@ -0,0 +1,107 @@
+export const jsQuizz = {
+    questions: [
+      {
+        question:
+          "What course is this?",
+        choices: [
+          "CSE2103",
+          "CSE3140",
+          "MATH3160",
+          "CSE2102",
+        ],
+        type: "MCQs",
+        correctAnswer: "CSE2102",
+      },
+      {
+        question: "How many blue stripes are there on the U.S. flag?",
+        choices: [
+          "6",
+          "7",
+          "0",
+          "13",
+        ],
+        type: "MCQs",
+        correctAnswer: "0",
+      },
+      {
+        question:
+          "Which number is a multiple of 10",
+        choices: ["63", "40", "73", "2"],
+        type: "MCQs",
+        correctAnswer: "40",
+      },
+      {
+        question: "The Agile method is not useful",
+        choices: ["True", "False"],
+        type: "MCQs",
+        correctAnswer: "False",
+      },
+      {
+        question: "I stands for Iteration in the SOLD principles ",
+        choices: [
+          "True",
+          "False",
+        ],
+        type: "MCQs",
+        correctAnswer: "False",
+      },
+      {
+        question: "What is 4 + 7?",
+        choices: [
+          "3",
+          "2",
+          "7",
+          "11",
+        ],
+        type: "MCQs",
+        correctAnswer: "11",
+      },
+      {
+        question: "Black and pink are the same color",
+        choices: [
+          "True",
+          "False",
+        ],
+        type: "MCQs",
+        correctAnswer: "False",
+      },
+      {
+        question: "Which of the following is not an IDE",
+        choices: [
+          "Pycharm",
+          "Visual Studio Code",
+          "IntelliJ",
+          "Skype",
+        ],
+        type: "MCQs",
+        correctAnswer: "Skype",
+      },
+      {
+        question: "Scrums should happen every 6 months only ",
+        choices: [
+          "True",
+          "False",
+        ],
+        type: "MCQs",
+        correctAnswer: "False",
+      },
+      {
+        question: "What is 5 - 3?",
+        choices: [
+          "4",
+          "2",
+          "8",
+          "10",
+        ],
+        type: "MCQs",
+        correctAnswer: "2",
+      },
+    ],
+  };
+
+export const resultInitialState = {
+  score: 0,
+  correctAnswers: 0,
+  wrongAnswers: 0
+
+}
